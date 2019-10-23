bfe09832b734fbbbffa6e496a1923c6a09309843
David Gruseck <david.gruseck@actinate.com>
2019-10-23 10:16:01 +0200

    add more vids

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 var videoSource = new Array();
-videoSource[0] = "./../videos/bigbig.mp4";
-videoSource[1] = "./../videos/bigsmall.mp4";
-videoSource[2] =
-  "http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4";
+videoSource[0] = "./../videos/VP8_720_10s_1MB.webm";
+videoSource[1] = "./../videos/VP8_720_10s_5MB.webm";
+videoSource[2] = "./../videos/VP8_720_10s_20MB.webm";
+videoSource[3] = "./../videos/VP8_1080_10s_1MB.webm";
+videoSource[4] = "./../videos/VP8_1080_10s_5MB.webm";
+videoSource[5] = "./../videos/VP8_1080_10s_10MB.webm";
+videoSource[6] = "./../videos/VP9_720_10s_1MB.webm";
+videoSource[7] = "./../videos/VP9_720_10s_5MB.webm";
+videoSource[8] = "./../videos/VP9_720_10s_10MB.webm";
+videoSource[9] = "./../videos/VP9_1080_10s_1MB.webm";
+videoSource[10] = "./../videos/VP9_1080_10s_5MB.webm";
+videoSource[11] = "./../videos/VP9_1080_10s_10MB.webm";
+videoSource[12] = "./../videos/bigbig.mp4";
+videoSource[13] = "./../videos/bigsmall.mp4";
+// videoSource[2] =
+//   "http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4";
 var videoCount = videoSource.length;
 var i = 0;
 document.getElementById("player").setAttribute("src", videoSource[0]);
 document.getElementById("player").play();
 
 function videoPlay(videoNum) {
+  document.getElementById("title").innerHTML = videoSource[videoNum];
   document.getElementById("player").setAttribute("src", videoSource[videoNum]);
   document.getElementById("player").play();
 }
